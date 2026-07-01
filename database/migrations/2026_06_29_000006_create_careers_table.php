f6b2fe6aaf3cdc82b7770e7bd9e1ce72c6b17f2b
Jazmanudin27 <jazmanudin9@gmail.com>
2026-07-01 09:21:46 +0700

    first commit

---
+++ database/migrations/2026_06_29_000006_create_careers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('company');
+            $table->text('description');
+            $table->string('type'); // fulltime, parttime, internship, freelance
+            $table->string('location');
+            $table->string('contact');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('careers');
+    }
+};
